8662255f81d446c9176b4ddd6886c001680ee7d9
Mohammed Al-Maqbli <Mohammed.almaqbli@gmail.com>
2021-05-15 10:26:09 +0300

    Create Apollo Server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,9 @@
-//TODO
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+
+const server = new ApolloServer({
+  typeDefs,
+});
+server
+  .listen()
+  .then(console.log("Server is running!🔉  Listening on port 4000"));
